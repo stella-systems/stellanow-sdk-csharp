8004d6b31f36a1d65686165a06b3f91799314efd
Tom Kandziora <tom@tomkandziora.com>
2023-06-20 17:23:19 +0100

    [STLND-618] - Updated licencing across the whole project to MIT.

--- StellaNowSDKDemo/src/Program.cs
+++ StellaNowSDKDemo/src/Program.cs
@@ -1,4 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// Copyright (C) 2022-2023 Stella Technologies (UK) Limited.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using StellaNowSDK.Config;
 using StellaNowSDK.Enums;
@@ -71,7 +91,7 @@ internal class Program
             {
                 ApiKey = "username10@some.domain",
                 ApiSecret = "1234567890",
-                ClientId = "StellaNowCsharpSDK",
+                ClientId = "StellaNowSDK",
                 OrganizationId = "62dbd729-54c0-43cd-9282-1828424f0873",
                 ProjectId = "0a1aae1e-b798-4a8a-a9f7-6aa521c6209d"
             }
--- StellaNowSDKTests/src/ConnectionStrategies/StellaNowMqttConnectionStrategyTests.cs
+++ StellaNowSDKTests/src/ConnectionStrategies/StellaNowMqttConnectionStrategyTests.cs
@@ -1,3 +1,23 @@
+// Copyright (C) 2022-2023 Stella Technologies (UK) Limited.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -34,7 +54,7 @@ public class StellaNowMqttConnectionStrategyTests
             {
                 ApiKey = "username10@some.domain",
                 ApiSecret = "1234567890",
-                ClientId = "StellaNowCsharpSDK",
+                ClientId = "StellaNowSDK",
                 OrganizationId = "23bd77b6-11c1-494d-8881-f636928ccf62",
                 ProjectId = "18d41262-07e5-4e8a-9b06-cc238d013d09"
             }
