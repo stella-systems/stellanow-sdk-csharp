54ca05be86c0b24b888e27a6174b1b9ff9e27f6d
Tom Kandziora <tom@tomkandziora.com>
2024-02-17 22:49:29 +0000

    [STLND-1236] C# SDK - version 0.3.0
    [STLND-1245] Updated copyright message across the codebase

--- StellaNowSDKTests/src/TestUtilities/UserUpdateMessage.cs
+++ StellaNowSDKTests/src/TestUtilities/UserUpdateMessage.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2022-2023 Stella Technologies (UK) Limited.
+// Copyright (C) 2022-2024 Stella Technologies (UK) Limited.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
