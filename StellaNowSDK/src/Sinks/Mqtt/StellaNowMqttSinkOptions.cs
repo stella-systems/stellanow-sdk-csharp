0cf5a1df0d5c6595d1bc7cce60fc5d72dc0ab5ef
Tom Kandziora <tom@tomkandziora.com>
2025-02-22 11:14:02 +0100

    [STLND-1849] - C# SDK 0.4.0 - Removed StellaNowMqttSinkOptions, as not used.

--- StellaNowSDK/src/Sinks/Mqtt/StellaNowMqttSinkOptions.cs
+++
@@ -1,9 +0,0 @@
-using StellaNowSDK.Sinks.Mqtt.ConnectionStrategy;
-
-namespace StellaNowSDK.Sinks.Mqtt;
-
-public record StellaNowMqttSinkOptions(
-    bool WithTls = true,
-    IMqttConnectionStrategy ConnectionStrategy = null
-    
-);
